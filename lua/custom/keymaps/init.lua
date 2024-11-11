c82721ab6cdbdd1ae5f09d12b70084a928187085
Thomas Burkhalter <new.mysteryman@gmail.com>
2024-11-11 22:49:11 +0100

    Add keymaps and plugins

--- lua/custom/keymaps/init.lua
+++ lua/custom/keymaps/init.lua
@@ -2,4 +2,5 @@ local map = function(keys, func, desc)
   vim.keymap.set('n', keys, func, { desc = desc })
 end
 
-map('<leader>gf', '<CMD>e <cfile><cr>', 'Create and [G]o to [F]ile')
+map('<leader>gf', '<CMD>e <cfile><CR>', 'Create and [G]o to [F]ile')
+map('<leader>vgf', '<CMD>vert sfind <Plug><cfile><CR>')
