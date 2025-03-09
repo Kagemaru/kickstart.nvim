ac722f99b442850cc78dcdd1f0a325eff62f3888
Thomas Burkhalter <new.mysteryman@gmail.com>
2025-03-09 19:56:39 +0100

    Update dadbod config

--- lua/custom/plugins/dadbod.lua
+++ lua/custom/plugins/dadbod.lua
@@ -1,8 +1,29 @@
 return {
   {
-    'tpope/vim-dadbod',
-    filetype = 'sql',
+    'kristijanhusak/vim-dadbod-ui',
+    dependencies = {
+      { 'tpope/vim-dadbod', filetype = 'sql', lazy = true },
+      { 'kristijanhusak/vim-dadbod-completion', ft = { 'sql', 'mysql', 'plsql' }, lazy = true },
+    },
+    cmd = {
+      'DBUI',
+      'DBUIToggle',
+      'DBUIAddConnection',
+      'DBUIFindBuffer',
+    },
+    init = function()
+      -- DBUI Config
+      vim.g.db_ui_use_nerd_fonts = 1
+
+      local psql = function(db)
+        return 'postgres://postgres:postgres@localhost:5432/' .. db
+      end
+
+      vim.g.dbs = {
+        { name = 'kpt_dev', url = psql 'key_point_tracker_dev' },
+        { name = 'kpt_test', url = psql 'key_point_tracker_test' },
+        { name = 'ptime_int', url = psql 'pitc_ptime_int' },
+      }
+    end,
   },
-  'kristijanhusak/vim-dadbod-completion',
-  'kristijanhusak/vim-dadbod-ui',
 }
